d29f5e81c96042065a4d29774455f199a4c0c058
Michael Caputi <31512924+caputim@users.noreply.github.com>
2017-10-05 17:09:10 -0400

    part 2

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/QuestionMania.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class QuestionMania : CarnivalGame
+    {
+        public QuestionMania() : base()
+        {
+
+        }
+
+        public override string getName()
+        {
+            return "Question Mania";
+        }
